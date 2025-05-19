1c17823b45a54315a62cbf9b7928054a2a4ca822
Vivek Thakur <itsvivekthakr@gmial.com>
2025-05-19 23:23:48 +0545

    added mad-libs_game.py

--- mad-libs_game.py
+++ mad-libs_game.py
@@ -1,3 +1,5 @@
+# Create a program that prompts users for words (nouns, verbs, adjectives) and inserts them into a story template. 
+
 def mad_libs():
     noun = input("Enter a noun: ")
     verb = input("Enter a verb: ")
