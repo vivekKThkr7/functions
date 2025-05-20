ca18f738d009b6372e622c4c2b240aeb68bc38e1
Vivek Thakur <itsvivekthakr@gmail.com>
2025-05-20 23:40:33 +0545

    Function with input parameters

---
+++ 1.py
@@ -0,0 +1,10 @@
+def greet(name):
+  print(f"Hello, {name}")
+
+greet("Mr. X")
+
+def greet_with_time(name, time="morning"):
+    print(f"Good {time}, {name}!")
+
+greet_with_time("Bob")  
+greet_with_time("Charlie", "evening")
